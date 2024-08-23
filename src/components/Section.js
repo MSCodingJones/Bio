b8eac72a941a42fc925e7a05e29c922394095488
MSCodingJones <sm.jones424@icloud.com>
2024-08-22 21:24:45 -0500

    first commit

---
+++ src/components/Section.js
@@ -0,0 +1,17 @@
+const Section =()=> {
+
+    let sectionNum = 1
+
+    return (
+        <section>
+            <h2>In The Beginning...Section { sectionNum }</h2>
+                <p>Memories</p>
+                    <p>"A good life is a collection of happy memories"</p>
+            <h2>Youthful Progression</h2>
+                <p>Being a Child</p>
+                    <p>"Youth is happy because it has the capacity to see beauty. Anyone who keeps the ability to see beauty never grows old"</p>
+        </section>
+    )
+}
+
+export default Section
